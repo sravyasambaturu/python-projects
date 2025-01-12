9149cf6f3b8f7ac5428237ec064057557f385003
sravyasambaturu <sravya.sambaturu75@gmail.com>
2025-01-12 15:14:47 -0500

    fifth one

---
+++ five/five.py
@@ -0,0 +1,14 @@
+# Write a Python program that accepts a word from the user and reverses it.
+
+my_string = input("Enter the word that needs to be reveresed: ")
+reversed_string = my_string[::-1]
+print(reversed_string)  
+
+
+#or 
+
+# word = input("Input a word to reverse: ")
+# for char in range(len(word) - 1, -1, -1):
+#     print(word[char], end="")
+
+# print("\n")
