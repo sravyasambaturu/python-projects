88cd79b089a3fa56325f97385ab16c0630512787
sravyasambaturu <sravya.sambaturu75@gmail.com>
2025-01-12 15:45:18 -0500

    update

---
+++ Conditional-Statements&Loops/six/six.py
@@ -0,0 +1,6 @@
+# Write a Python program to count the number of even and odd numbers in a series of numbers
+# Sample numbers : numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
+# Expected Output :
+# Number of even numbers : 5
+# Number of odd numbers : 4
+
