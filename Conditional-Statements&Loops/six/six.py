4630c3116846620a9005c71851a7c43739dbe78f
sravyasambaturu <sravya.sambaturu75@gmail.com>
2025-01-12 17:50:56 -0500

    i progressed a bit

--- Conditional-Statements&Loops/six/six.py
+++ Conditional-Statements&Loops/six/six.py
@@ -4,3 +4,32 @@
 # Number of even numbers : 5
 # Number of odd numbers : 4
 
+
+input_numbers = input("enter the sample numbers: ")
+seperated_numbers = input_numbers.split(",")
+updated_numbers = [int(num.strip()) for num in seperated_numbers]
+even_numbers = []
+odd_numbers = []
+for i in updated_numbers:
+    if i%2==0:
+        even_numbers.append(i)
+    else:
+        odd_numbers.append(i)
+print("Number of even numbers: " + str(len(even_numbers)))
+print("Number of odd numbers: " + str(len(odd_numbers)))
+ 
+    
+#or
+
+# numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+# count_odd = 0
+# count_even = 0
+# for x in numbers:
+#     if not x % 2: 
+#         count_even += 1
+#     else:
+#         count_odd += 1
+# print("Number of even numbers:", count_even)
+# print("Number of odd numbers:", count_odd) 
+
+
